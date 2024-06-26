6c318d6c180c7717e7a7bab7959cb128bb0945e2
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-26 16:22:25 +0100

    Create a ModelContainer for DataItem type

--- SwiftDataDemo/SwiftDataDemoApp.swift
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataDemoApp: App {
@@ -13,5 +14,6 @@ struct SwiftDataDemoApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: DataItem.self)
     }
 }
