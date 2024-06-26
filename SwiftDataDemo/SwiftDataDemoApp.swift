09efe63af07f1b0539dc6bf461091ca240af5469
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-26 16:04:04 +0100

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Sunjay Kalsi on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
