94911b17d8a2187929fce8ea835ea4cfd087a07a
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-29 07:42:13 +0100

    code

--- SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var context
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tap on this button to add data")
+            Button("Add an item") {
+                addItem()
+            }
         }
         .padding()
+    }
+
+    func addItem () {
+        // create item
+        let item = DataItem(name: "Test Item")
+        
+        // Add the item to the data context
+        context.insert(item)
     }
 }
 
